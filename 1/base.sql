4f2c7b6d5f1c447edd20879f3063eacc1f376da5
Hanafi Adhi <TFJ-187@macbooks-MacBook-Pro.local>
2026-07-01 16:58:23 +0700

    report baru
    Signed-off-by: Hanafi Adhi <TFJ-187@macbooks-MacBook-Pro.local>

--- 1/base.sql
+++ 1/base.sql
@@ -4,21 +4,22 @@ WITH paid_subs AS (
         dc.id AS channel_id,
         dc.title AS channel_name,
 --         dc.price as channel_price,
-        dp.cust_no AS customer_id,
-        dp.cust_name AS customer_name,
+        dsu.user_id AS customer_id,
+        u.fullname AS customer_name,
         dsu.CREATED_AT AS sub_created_at,
         dsu.EXPIRED AS sub_expired,
         ROW_NUMBER() OVER (
-            PARTITION BY dp.cust_no, dsu.channel_id
+            PARTITION BY dsu.user_id, dsu.channel_id
             ORDER BY dsu.CREATED_AT DESC
         ) AS rn,
         IFNULL(CONVERT_TZ(mrg.last_trade, '+00:00', '+07:00'), '') AS last_trade_jakarta
     FROM dsc_subs dsu
     INNER JOIN dsc_channels dc ON dsu.channel_id = dc.id
-    INNER JOIN dsc_payment dp ON dsu.paid = dp.bill_no
-    INNER JOIN dsc_subs_details dsd ON dsd.subs_id = dsu.id
+    INNER JOIN users u ON dsu.user_id = u.id
     LEFT JOIN affiliate_mrg_users_stats mrg ON dsu.user_id = mrg.mrg_id
-    WHERE dc.price != 0
+    WHERE mrg.last_trade IS NOT NULL -- analis aktif
+    	AND dsu.paid = 0 -- paid
+--     	AND dsu.paid != 0 -- free
    )
    
 SELECT ps.customer_id, ps.customer_name, JSON_ARRAYAGG(
--- 2/base.sql
+++ 2/base.sql
@@ -1,9 +1,10 @@
-WITH free_subs AS (
-	 SELECT
+WITH paid_subs AS (
+    -- PAID SUBS
+    SELECT
         dc.id AS channel_id,
         dc.title AS channel_name,
 --         dc.price as channel_price,
-        u.id AS customer_id,
+        dsu.user_id AS customer_id,
         u.fullname AS customer_name,
         dsu.CREATED_AT AS sub_created_at,
         dsu.EXPIRED AS sub_expired,
@@ -11,14 +12,16 @@ WITH free_subs AS (
             PARTITION BY dsu.user_id, dsu.channel_id
             ORDER BY dsu.CREATED_AT DESC
         ) AS rn,
-    	IFNULL(CONVERT_TZ(mrg.last_trade, '+00:00', '+07:00'), '') AS last_trade_jakarta
+        IFNULL(CONVERT_TZ(mrg.last_trade, '+00:00', '+07:00'), '') AS last_trade_jakarta
     FROM dsc_subs dsu
     INNER JOIN dsc_channels dc ON dsu.channel_id = dc.id
     INNER JOIN users u ON dsu.user_id = u.id
     LEFT JOIN affiliate_mrg_users_stats mrg ON dsu.user_id = mrg.mrg_id
-    WHERE dc.price = 0
-) 
-
+    WHERE mrg.last_trade IS NULL -- analis tidak aktif
+    	AND dsu.paid = 0 -- paid
+--     	AND dsu.paid != 0 -- free
+   )
+   
 SELECT ps.customer_id, ps.customer_name, JSON_ARRAYAGG(
         JSON_OBJECT(
             'channel_id', ps.channel_id,
@@ -28,6 +31,6 @@ SELECT ps.customer_id, ps.customer_name, JSON_ARRAYAGG(
         )
     ) AS list_channels,
     ps.last_trade_jakarta
-FROM free_subs ps
-WHERE ps.rn =1
+FROM paid_subs ps
+WHERE ps.rn = 1
 GROUP BY ps.customer_id, ps.customer_name
